67dc04fb2d6dcc38b007b64b36391649edea7476
Anner-0 <anner.calvo@gmail.com>
2020-09-23 18:29:46 -0600

    TareaCorta3-AnnerCalvo

--- sumatoria.py
+++ sumatoria.py
@@ -1,6 +1,15 @@
 def sumatoria_cubica(n):
-    raise NotImplementedError()
+    result=0
+    for i in range(1, n+1):
+        for j in range(1, i+1):
+            for k in range(j, i+j+1):
+                result+=1
+    return(result)
+    # raise NotImplementedError()
 
 def sumatoria_constante(n):
-    raise NotImplementedError()
+    result=((n*(n+1)*(2*n+1))/6)+((n*(n+1))/2)
+    return(result)
+    # raise NotImplementedError()
+
 
